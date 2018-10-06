be41b6d35cbb5dc37e2bafb8ed1f981714f143a3
Hanna <hannaseithe@googlemail.com>
2018-10-06 18:04:43 +0200

    Background Color + InlineScript Error

--- src2/app/app.shared-material.module.ts
+++ src2/app/app.shared-material.module.ts
@@ -1,4 +1,4 @@
-import { MatButtonModule, MatAutocompleteModule, MatInputModule, MatCardModule, MatFormFieldModule, MatIconModule, MatTableModule, MatDialogModule, MatToolbarModule, MatChipsModule, MatTooltipModule, MatCheckboxModule, MatPaginatorModule, MatExpansionModule, MatSnackBarModule, MatProgressSpinnerModule, MatSortModule, MatGridListModule } from '@angular/material';
+import { MatButtonModule, MatAutocompleteModule, MatInputModule, MatCardModule, MatFormFieldModule, MatIconModule, MatTableModule, MatDialogModule, MatToolbarModule, MatChipsModule, MatTooltipModule, MatCheckboxModule, MatPaginatorModule, MatExpansionModule, MatSnackBarModule, MatProgressSpinnerModule, MatSortModule, MatGridListModule, MatDividerModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 @NgModule({
@@ -21,6 +21,7 @@ import { NgModule } from '@angular/core';
         MatSnackBarModule,
         MatProgressSpinnerModule,
         MatSortModule,
+        MatDividerModule
     ],
     exports: [
         MatIconModule,
@@ -41,6 +42,7 @@ import { NgModule } from '@angular/core';
         MatSnackBarModule,
         MatProgressSpinnerModule,
         MatSortModule,
+        MatDividerModule
     ],
 })
 export class SharedMaterialModule { }
